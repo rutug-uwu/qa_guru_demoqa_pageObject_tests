284972c77378eed599b0a4f52a395d65bf02f48e
Ruslan Tugushev <rutug.official@gmail.com>
2025-07-06 23:39:20 +0400

    Скорректировал названия класса RegistrationWithPageObjectTests с тестами и добавил новый класс RegistrationWithDataTests с тестами, где будет использоваться геренация тестовых данных

---
+++ src/test/java/tests/RegistrationWithDataTests.java
@@ -0,0 +1,70 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import pages.RegistrationPage;
+
+public class RegistrationWithDataTests extends TestBase {
+
+    RegistrationPage registrationPage = new RegistrationPage();
+
+
+    @Test
+    void successfulRegistrationTest() {
+        registrationPage.openPage()
+                .hideAd()
+                .setFirstName("Ivan")
+                .setSecondName("Ivanov")
+                .setEmail("Ivanov@123.com")
+                .setPhone("89999990000")
+                .setCurrentAddress("Some address 123")
+                .setGender()
+                .setDateOfBirth("24", "February", "1999")
+                .setSubject("Physics")
+                .setSubject("English")
+                .setHobbies()
+                .uploadPicture("Img/photo_2022-12-19_20-56-33.jpg")
+                .setStateAndCity("NCR", "Delhi")
+                .clickSubmit();
+
+        registrationPage.verifyResultsModalAppears("Thanks for submitting the form")
+                .verifyResult("Student Name", "Ivan Ivanov")
+                .verifyResult("Student Email", "Ivanov@123.com")
+                .verifyResult("Gender", "Male")
+                .verifyResult("Mobile", "8999999000")
+                .verifyResult("Date of Birth", "24 February,1999")
+                .verifyResult("Subjects", "Physics, English")
+                .verifyResult("Hobbies", "Sports")
+                .verifyResult("Picture", "photo_2022-12-19_20-56-33.jpg")
+                .verifyResult("Address", "Some address 123")
+                .verifyResult("State and City", "NCR Delhi");
+    }
+
+    @Test
+    void successfulRegistrationMinimalTest() {
+        registrationPage.openPage()
+                .hideAd()
+                .setFirstName("Oleg")
+                .setSecondName("Olegov")
+                .setGender()
+                .setPhone("8777777777")
+                .clickSubmit();
+
+        registrationPage.verifyResultsModalAppears("Thanks for submitting the form")
+                .verifyResult("Student Name", "Oleg Olegov")
+                .verifyResult("Gender", "Male")
+                .verifyResult("Mobile", "8777777777");
+    }
+
+    @Test
+    void negativeRegistrationTest() {
+        registrationPage.openPage()
+                .hideAd()
+                .setFirstName("Oleg")
+                .setSecondName("Olegov")
+                .setGender()
+                .setPhone("8777777777")
+                .clickSubmit();
+
+        registrationPage.verifyResultsModalNotAppears();
+    }
+}
--- src/test/java/tests/RegistrationWithPageObjectTests.java
+++ src/test/java/tests/RegistrationWithPageObjectTests.java
@@ -3,7 +3,7 @@ package tests;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
-public class RegistrationTests extends TestBase {
+public class RegistrationWithPageObjectTests extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
 
