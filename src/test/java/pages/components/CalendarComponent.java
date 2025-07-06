e7c4ce10a4081b12ddef3ba91c9095e6242e6509
Ruslan Tugushev <rutug.official@gmail.com>
2025-07-06 22:57:15 +0400

    Вынес в классе CalendarComponent все локаторы в переменные

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -1,16 +1,20 @@
 package pages.components;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class CalendarComponent {
     private final SelenideElement monthSelect = $(".react-datepicker__month-select"),
             yearSelect = $(".react-datepicker__year-select");
+    private final ElementsCollection daySelect = $$(".react-datepicker__day");
 
     public void setDate(String day, String month, String year) {
         monthSelect.selectOption(month);
         yearSelect.selectOption(year);
-        $(".react-datepicker__day--0" + day).click();
+        daySelect.findBy(text(day)).click();
     }
 }
