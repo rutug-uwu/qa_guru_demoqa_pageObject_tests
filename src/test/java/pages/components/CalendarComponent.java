06746b005fc659f19a3f39bd757fe18b8a1d6ee3
Ruslan Tugushev <rutug.official@gmail.com>
2025-07-09 01:46:03 +0400

    Добавлен автотест с библиотекой Faker -> RegistrationWithDataTests + добавлен класс, генерирующий тестовые данные -> TestData

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -1,11 +1,6 @@
 package pages.components;
 
-import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.SelenideElement;
-
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
 
 public class CalendarComponent {
     public void setDate(String day, String month, String year) {
